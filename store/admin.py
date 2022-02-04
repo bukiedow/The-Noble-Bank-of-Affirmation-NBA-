e036436144d21f49f41bd1ea78dcdeba42d92132
bukiedow <dowbukie@gmail.com>
2022-02-04 04:21:31 +0000

    django admin customization

--- store/admin.py
+++ store/admin.py
@@ -4,5 +4,23 @@ from .models import Product, Collection
 
 # Register your models here.
 
-admin.site.register(Product)
-admin.site.register(Collection)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'collection',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Collection, CollectionAdmin)
