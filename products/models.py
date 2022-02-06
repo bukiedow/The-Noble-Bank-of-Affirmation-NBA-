171de60e75f277eca16ead17e41bccc955b79661
bukiedow <dowbukie@gmail.com>
2022-02-06 06:46:30 +0000

    added new field and updated description  to product json file

--- products/models.py
+++ products/models.py
@@ -21,6 +21,7 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    lookAfterMe = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
