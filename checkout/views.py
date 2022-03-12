2a7ea4864bdef8202a9453beef92c33ac5c2b677
bukiedow <dowbukie@gmail.com>
2022-03-12 20:47:57 +0000

    strioe functionality added tp checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KRNtoJuRvdA0dOiWScxBErGPkEW9jux7yGfNvC8RMkpCvMgXy8erKXMfQthSeLaMJYmA8zBcjER3CG9NNgP26HT005Cj8laDO',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
